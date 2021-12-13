78cdf4f084a1cc2552aef061ef650fd2cb7c60ac
Raj Singh <rajkumar@technogramsolutions.com>
2021-12-13 12:27:28 +0530

    UPDATE: Images Upload & Preview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  useEffect(() => {
+    if (images.length < 1) return;
+    const newImageUrls = [];
+    images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
+    setImageUrls(newImageUrls);
+  }, [images])
+
+  const onImageChange = (event) => {
+    setImages([...event.target.files])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="image_upload-wrap">
+          <div className="image_uplaod-body">
+            <h3 className="image_uplaod-heading">UPLOAD IMAGES</h3>
+            <input type="file"
+              className="image_uplaod-input"
+              onChange={(e) => onImageChange(e)}
+              multiple
+            />
+            <div className="image_uplaod--imgwrap">
+              {imageUrls.map((imgSrc, key) =>
+                <div className="image_uplaod-flex">
+                  <img className="image_uplaod--images" key={key} src={imgSrc} />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
